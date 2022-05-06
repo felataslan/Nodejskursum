5f7a9951faccee37e9e35aee2b8d4ef3d5a4129b
felat aslan <felataslan58@gmail.com>
2022-05-06 23:20:32 +0300

    Node JS kursu 1.gün

---
+++ 2-temel-JS/degiskenler.js
@@ -0,0 +1,6 @@
+var data = 2,
+ adsoyad="felat aslan";
+ db="mongodb";
+console.log(data);
+console.log(adsoyad);
+console.log(db);
---
+++ 2-temel-JS/kucukbuyukduyarlılıgı.js
@@ -0,0 +1,10 @@
+var ad ="mehmet",
+ad1="Mehmet";
+//sart kosulları ile büyük küçük harf duyarlılığını kontrol etmek
+if(ad==ad1){
+    console.log("büyük küçük harf duyarlılığı yoktur");
+
+}else{
+    console.log("buyuk küçük harf duyarlılığı vardır");
+    
+}
